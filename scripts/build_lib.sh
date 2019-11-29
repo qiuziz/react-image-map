024cb78900a0c4a9916a86ed69fb4951ead213f9
qiuz <zhu_anly@163.com>
2019-11-29 17:10:50 +0800

    build_lib npm version first

--- scripts/build_lib.sh
+++ scripts/build_lib.sh
@@ -5,7 +5,7 @@ set -e
 function build() {
 	version=$1
 
-	VERSION=npm version ${version:-patch}
+	VERSION=`npm version ${version:-patch}`
 	echo $VERSION
 	# if [[ -n "$git_revision" ]]; then
 	# 	current_branch="$(git symbolic-ref --short -q HEAD)"
