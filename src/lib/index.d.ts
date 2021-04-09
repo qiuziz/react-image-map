af90428c04a879159bbea7b4d431fcb25d78dde9
qiuz <zhu_anly@163.com>
2021-04-09 15:02:28 +0800

    **ImageMap npm build**

--- src/lib/index.d.ts
+++ src/lib/index.d.ts
@@ -3,13 +3,13 @@
  * @Github: <https://github.com/qiuziz>
  * @Date: 2019-11-27 22:05:04
  * @Last Modified by: qiuz
- * @Last Modified time: 2021-04-09 14:37:06
+ * @Last Modified time: 2021-04-09 14:54:35
  */
 
 import * as React from 'react';
 
 export interface Area
-  extends React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLSpanElement>, HTMLSpanElement> {
+  extends React.DetailedHTMLProps<React.SpanHTMLAttributes<HTMLSpanElement>, HTMLSpanElement> {
   left: string;
   top: string;
   height: string;
