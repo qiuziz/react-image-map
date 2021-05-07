610bbaa96aff6fdde04a2579339ade782fe58d6a
qiuz <zhu_anly@163.com>
2021-05-07 09:40:32 +0800

    docs: update usage

--- src/component/image-map/index.d.ts
+++ src/component/image-map/index.d.ts
@@ -3,7 +3,7 @@
  * @Github: <https://github.com/qiuziz>
  * @Date: 2019-11-27 22:05:04
  * @Last Modified by: qiuz
- * @Last Modified time: 2021-04-09 14:54:35
+ * @Last Modified time: 2021-05-07 09:34:26
  */
 
 import * as React from 'react';
@@ -14,6 +14,7 @@ export interface Area
   top: string;
   height: string;
 	width: string;
+	style?: React.CSSProperties;
 	render?: (area: Area, index: number) => React.ReactNode;
 }
 
